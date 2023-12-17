5700eb01c196411dee6132055a9a1967a6013cf2
Junpeng <junpenggaox@gmail.com>
2023-12-17 21:41:04 +0100

    fix energy

--- implicit_soft_body/Sim.py
+++ implicit_soft_body/Sim.py
@@ -29,7 +29,7 @@ class DiffSim(torch.autograd.Function):
             optimizer.step(closure)
             if torch.norm(dx-dx0)/dx0.norm() < 1e-3:
                 break
-        print(f'epoch {epoch_i}: loss {dx.norm().item()}, relative loss change {torch.norm(dx-dx0)/dx0.norm()}')
+        # print(f'epoch {epoch_i}: loss {dx.norm().item()}, relative loss change {torch.norm(dx-dx0)/dx0.norm()}')
         v = dx/dt
         x = x0+dx
         ctx.save_for_backward(x0, x, v0, v, a)
@@ -38,14 +38,14 @@ class DiffSim(torch.autograd.Function):
     
     @staticmethod
     def backward(ctx, grad_output_x, grad_output_v) -> Any:
-        print("backward")
+        # print(grad_output_x)
         
         x0, x, v0, v, a = ctx.saved_tensors
         x = x.detach().clone().requires_grad_(True)
         a = a.detach().clone().requires_grad_(True)
         def energy_partial(x):
             x = x.reshape(-1,2)
-            return ctx.robot.total_energy(x0, x, v0, a)
+            return ctx.robot.potential_energy(x0, x, v0, a)
         with torch.enable_grad():
             x_flat = x.flatten()
             E = energy_partial(x_flat)
@@ -53,11 +53,20 @@ class DiffSim(torch.autograd.Function):
         dLdx = grad_output_x
         dLdx = dLdx.flatten()
         dfdx = -torch.autograd.functional.hessian(energy_partial, x_flat)
-        dfdx = torch.transpose(dfdx, 0, 1)
+        # print("eigen of dfdx", torch.linalg.eigvals(dfdx))
+        # print("dfdx ", dfdx)
+        # print("dLdx ", dLdx)
         z = torch.linalg.solve(dfdx, dLdx)
+        # print("z ", z)
+        # print("f ", f)
+        # print(f)
+        # print(z)
 
         with torch.enable_grad():
             # f_flat = f.flatten()
-            zTf = z.reshape(1,-1) @ f
+            # zTf = z.reshape(1,-1) @ f
+            zTf = torch.dot(z, f)
+            # print("zTf ", zTf)
             dLda = -torch.autograd.grad(zTf, a)[0]
+            # print("dLda ", dLda)
         return None, None, dLda, None, None, None
